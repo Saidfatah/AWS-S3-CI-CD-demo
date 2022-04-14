cc3c6d2c0314ed1d4d1ec3bb64cae01851650fd2
SaidFatah <said_designer@outlook.com>
2022-04-14 20:08:08 +0000

    implemnted CI/CD integration using github actions to sync repo with aws s3 bucket

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Nice I'm finnaly learning aws 
         </a>
       </header>
     </div>
